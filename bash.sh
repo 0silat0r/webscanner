085f321c353d3e2f1b21c1cc4e0d4d33b0e843c1
Osilator <87228325+0silat0r@users.noreply.github.com>
2025-05-28 15:51:06 -0400

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,7 @@
+sudo apt update -y;
+sudo apt upgrade -y;
+sudo apt install python3 -y;
+sudo apt install python -y;
+pip install requests;
+pip install sockets;
+echo "Ana kurulumlar tamamlandi. 'python3 program.py' komutunu kullanarak programi calistirabilirsiniz."
